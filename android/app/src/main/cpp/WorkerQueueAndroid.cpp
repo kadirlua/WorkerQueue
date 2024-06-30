f8b0eb8e68b9d7a3a16479792626fe17780b3400
Kadir <kadir_altindag@outlook.com>
2024-07-01 00:08:32 +0300

    Improved android sample application

--- android/app/src/main/cpp/WorkerQueueAndroid.cpp
+++ android/app/src/main/cpp/WorkerQueueAndroid.cpp
@@ -1,19 +1,40 @@
 #include <jni.h>
+#include <cstdlib>
+#include <filesystem>
 #include <string>
+#include <vector>
 #include <concurrency/WorkerQueue.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_sdk_workerqueue_MainActivity_stringFromJNI(
+namespace {
+    constexpr const auto DEFAULT_SLEEP_TIME = 100;
+}
+
+extern "C" JNIEXPORT jobjectArray JNICALL
+Java_com_sdk_workerqueue_MainActivity_listDirectoryContents(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string result;
+        jobject /* this */,
+        jstring path) {
+    const char *pathStr = env->GetStringUTFChars(path, nullptr);
+
     sdk::concurrency::WorkerQueue wQueue;
-    wQueue.push([&result](){
-        result.append("Test message from worker queue!");
+    std::vector<std::string> files;
+    wQueue.push([&files, pathStr](){
+        namespace fs = std::filesystem;
+
+        for (const auto& entry : fs::directory_iterator(pathStr)) {
+            files.push_back(entry.path().filename().string());
+        }
     });
     // wait until worker queue has finished
-	while (!wQueue.empty()) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	}
-    return env->NewStringUTF(result.c_str());
+    while (!wQueue.empty()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(DEFAULT_SLEEP_TIME));
+    }
+
+    jobjectArray fileArray = env->NewObjectArray(files.size(), env->FindClass("java/lang/String"), nullptr);
+    for (std::size_t i = 0; i < files.size(); ++i) {
+        env->SetObjectArrayElement(fileArray, i, env->NewStringUTF(files[i].c_str()));
+    }
+
+    env->ReleaseStringUTFChars(path, pathStr);
+    return fileArray;
 }
--- android/app/src/main/java/com/sdk/workerqueue/MainActivity.java
+++ android/app/src/main/java/com/sdk/workerqueue/MainActivity.java
@@ -2,8 +2,18 @@ package com.sdk.workerqueue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import java.util.ArrayList;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.Intent;
+import android.net.Uri;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.documentfile.provider.DocumentFile;
 
 import com.sdk.workerqueue.databinding.ActivityMainBinding;
 
@@ -16,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> fileList;
+
+    // Register the launcher for activity result
+    private final ActivityResultLauncher<Intent> directoryPickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    if (uri != null) {
+                        DocumentFile directory = DocumentFile.fromTreeUri(this, uri);
+                        if (directory != null && directory.isDirectory()) {
+                            fileList.clear();
+                            for (DocumentFile file : directory.listFiles()) {
+                                fileList.add(file.getName());
+                            }
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +56,25 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        Button reqBtn = binding.buttonBrowse;
+        ListView listViewDir = binding.listViewDir;
+
+        fileList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fileList);
+        listViewDir.setAdapter(adapter);
+
+        reqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                directoryPickerLauncher.launch(intent);
+            }
+        });
     }
 
     /**
      * A native method that is implemented by the 'WorkerQueue' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String[] listDirectoryContents(String path);
 }
