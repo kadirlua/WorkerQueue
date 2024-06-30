76d1729bc2fd94848942ebe69393cec5453e821d
Kadir <kadir_altindag@outlook.com>
2024-06-30 13:58:15 +0300

    Improve Android sample

--- android/app/src/main/cpp/WorkerQueueAndroid.cpp
+++ android/app/src/main/cpp/WorkerQueueAndroid.cpp
@@ -7,12 +7,13 @@ Java_com_sdk_workerqueue_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string result;
-    {
-        sdk::concurrency::WorkerQueue wQueue;
-        wQueue.push([&result](){
-            result.append("Test Message!");
-        });
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    sdk::concurrency::WorkerQueue wQueue;
+    wQueue.push([&result](){
+        result.append("Test message from worker queue!");
+    });
+    // wait until worker queue has finished
+	while (wQueue.getQueueSize() != 0) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
     return env->NewStringUTF(result.c_str());
 }
