618be275cb8b33a0598582f07ebb24e4ef140d60
Kadir <kadir_altindag@outlook.com>
2025-02-06 18:20:51 +0300

    Fix include order and add missing headers for functional and chrono

--- examples/main.cpp
+++ examples/main.cpp
@@ -20,10 +20,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <functional>
+#include <chrono>
 #include <iostream>
-#if (__cplusplus >= 201703L)
-#include <filesystem>
-#endif
+#include <thread>
 #include <concurrency/WorkerQueue.h>
 
 using namespace sdk::concurrency;
